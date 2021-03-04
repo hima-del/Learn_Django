30608362ac27b37c49977ccf607e78d30514b230
hima-del <himajaharidas77@gmail.com>
2021-03-04 19:22:16 +0530

    inittial commit

--- myproject/webapp/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import employess
-
-class employeeSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model=employess
-        fields='__all__'
